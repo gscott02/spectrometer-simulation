e3f6de8bbc84407e511b3ff8ec6395dea86b9732
gscott02 <gscott02@calpoly.edu>
2020-06-07 12:27:16 -0700

    Cleanup

--- src/components/LineWithLine.vue
+++ src/components/LineWithLine.vue
@@ -13,7 +13,7 @@
             Chart.controllers.line.prototype.draw.call(this, ease);
 
             if (this.chart.tooltip._active && this.chart.tooltip._active.length) {
-                var activePoint = this.chart.tooltip._active[0],
+                let activePoint = this.chart.tooltip._active[0],
                     ctx = this.chart.ctx,
                     x = activePoint.tooltipPosition().x,
                     topY = this.chart.scales['y-axis-0'].top,
